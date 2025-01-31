333da84f0e5b5fd212db837c9c582cd874a08ada
ratchetfreak <ratchet.freak@gmail.com>
2025-01-31 20:10:17 +0100

    do some half work, adding should work now (untested) now on to removing

--- ratbuf_btree.h
+++ ratbuf_btree.h
@@ -4,6 +4,8 @@
 #include <variant>
 #include <vector>
 #include <array>
+#include <string>
+#include <forward_list>
 
 
 #include "types.h"
@@ -11,22 +13,26 @@
 
 namespace PieceTree
 {
-    enum class BufferIndex : size_t
+    
+    struct BufferCollection;
+    using Editor::CharOffset;
+    using Editor::Length;
+    using Editor::Column;
+    struct NodeData;
+    enum class LFCount : size_t { };
+        enum class BufferIndex : size_t
     {
         ModBuf = sentinel_for<BufferIndex>
     };
-    enum class Line : size_t
+
+        enum class Line : size_t
     {
         IndexBeginning,
         Beginning
     };
-
-    using Editor::CharOffset;
-    using Editor::Length;
-    using Editor::Column;
-
-    enum class LFCount : size_t { };
-
+    template <size_t MaxChildren>
+    class B_Tree ;
+    
     struct BufferCursor
     {
         // Relative line in the current buffer.
@@ -36,6 +42,7 @@ namespace PieceTree
 
         bool operator==(const BufferCursor&) const = default;
     };
+
     struct Piece
     {
         BufferIndex index = { }; // Index into a buffer in PieceTree.  This could be an immutable buffer or the mutable buffer.
@@ -51,15 +58,20 @@ namespace PieceTree
     {
         PieceTree::Piece piece;
 
+        PieceTree::Length left_subtree_length = { };
+        PieceTree::LFCount left_subtree_lf_count = { };
     };
     
     template <size_t MaxChildren>
     class B_Tree 
     {
 
+        
+    public:
         struct Node;
         using NodePtr = std::shared_ptr<const Node>;
-        
+        using ChildArray =std::array<NodePtr, MaxChildren>;
+        using LeafArray =std::array<NodeData, MaxChildren>;
         struct Node :public std::enable_shared_from_this<Node>
         {
             Node(std::array<NodePtr, MaxChildren> chld, 
@@ -80,9 +92,9 @@ namespace PieceTree
             {
                 
             } 
-            std::variant<std::array<NodePtr, MaxChildren>,
-                        std::array<NodeData, MaxChildren> > children;
+            std::variant<ChildArray, LeafArray > children;
             std::array<Length, MaxChildren> offsets;
+            std::array<LFCount, MaxChildren> lineFeeds;
             size_t childCount;
             
             bool isLeaf ()const{
@@ -93,8 +105,11 @@ namespace PieceTree
             {
                 return offsets.back();
             }
+            LFCount subTreeLineFeeds() const
+            {
+                return lineFeeds.back();
+            }
         };
-    public:
         explicit B_Tree() = default;
         
         const Node* root_ptr() const;
@@ -105,22 +120,26 @@ namespace PieceTree
         bool operator==(const B_Tree&) const = default;
 
         // Mutators.
-        B_Tree insert(const NodeData& x, Offset at) const;
+        B_Tree insert(const NodeData& x, Offset at, BufferCollection* buffers) const;
+        B_Tree construct_from(std::vector<NodeData> leafNodes) const;
         B_Tree remove(Offset at) const;
         B_Tree removeRange(Offset at, Length len) const;
     private:
         B_Tree(NodePtr root);
         struct TreeManipResult{
             std::variant<std::vector<NodePtr>,std::vector<NodeData> > children;
-            std::vector<Length> offsets;
-            bool preserved_invariant;
+            size_t violated_invariant;
+            
+            std::vector<TreeManipResult> invalid;
         };
-        TreeManipResult insertInto(const Node* node, const NodeData& x, Length at) const;
-        TreeManipResult insertInto_leaf(const Node* node, const NodeData& x, Length at) const;
-        NodePtr construct_leaf(std::vector<NodeData> data, std::vector<Length> offsets,size_t begin, size_t end) const ;
-        NodePtr construct_internal(std::vector<NodePtr> data, std::vector<Length> offsets, size_t begin, size_t end) const ;
+        TreeManipResult insertInto(const Node* node, const NodeData& x, Length at, BufferCollection* buffers) const;
+        TreeManipResult insertInto_leaf(const Node* node, const NodeData& x, Length at, BufferCollection* buffers) const;
+        NodePtr construct_leaf(std::vector<NodeData> data, size_t begin, size_t end) const ;
+        NodePtr construct_internal(std::vector<NodePtr> data, size_t begin, size_t end) const ;
         
         NodePtr root_node;
+        
+        
     };
     
     template<size_t C>
@@ -157,21 +176,5 @@ namespace PieceTree
         std::vector<StackEntry> stack;
     };
     
-    struct WalkSentinel { };
-
-    template<size_t C>
-    inline B_TreeWalker<C> begin(const B_Tree<C>& tree)
-    {
-        return B_TreeWalker{ &tree };
-    }
-    template<size_t C>
-    constexpr WalkSentinel end(const B_Tree<C>&)
-    {
-        return WalkSentinel{ };
-    }
-    template<size_t C>
-    inline bool operator==(const B_TreeWalker<C>& walker, WalkSentinel)
-    {
-        return walker.exhausted();
-    }
-}
+    
+};
