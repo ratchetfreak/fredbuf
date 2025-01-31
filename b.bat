956d2e24497e55c2fcc811cc6ee55d75cf6b70ab
ratchetfreak <ratchet.freak@gmail.com>
2025-01-31 20:12:46 +0100

    untested insert added

--- b.bat
+++ b.bat
@@ -1,1 +1,3 @@
 cl /std:c++latest /EHsc /W4 /WX /diagnostics:caret /diagnostics:color /Zi fredbuf-test.cpp /Fefredbuf-test.exe
+
+cl /std:c++latest /EHsc /W4 /WX /diagnostics:caret /diagnostics:color /Zi ratbuf_btree.cpp /c
