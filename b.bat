8d2c5bfb0e32b86c5b3171e461beb4b26676d3f7
ratchetfreak <ratchet.freak@gmail.com>
2025-01-31 20:12:46 +0100

    untested insert added

--- b.bat
+++ b.bat
@@ -1,4 +1,3 @@
-
 setlocal enabledelayedexpansion
 
 for %%a in (%*) do set "%%a=1"
@@ -8,4 +7,4 @@ if "%timing%"=="1" set timing_flag=/DTIMING_DATA
 
 cl /nologo /std:c++latest /EHsc /W4 /WX /diagnostics:caret /diagnostics:color /Z7 %timing_flag% fredbuf-test.cpp /Fefredbuf-test.exe
 
-cl /c /std:c++latest /EHsc /W4 /WX /diagnostics:caret /diagnostics:color %timing_flag%ratbuf.cpp
+cl /nologo /std:c++latest /EHsc /W4 /WX /diagnostics:caret /diagnostics:color /Zi ratbuf_btree.cpp /c
