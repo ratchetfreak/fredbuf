d67256e87fbfa8ca20acd16009e022a6cb511228
ratchetfreak <ratchet.freak@gmail.com>
2025-10-28 08:26:35 +0100

    add some bitmagic prep

--- ratbuf_btree.cpp
+++ ratbuf_btree.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <array>
 #include <sstream>
+#include <bit>
 
 #include "types.h"
 #include "enum-utils.h"
@@ -533,6 +534,34 @@ namespace RatchetPieceTree
 
         return result;
 
+    }
+
+    template<typename It, typename T, typename Cmp>
+    It branchless_lower_bound(It begin, It end, const T & value, Cmp && compare)
+    {
+        size_t length = end - begin;
+        if (length == 0)
+            return end;
+        size_t step = std::bit_floor(length);
+        if (step != length && compare(begin[step], value))
+        {
+            length -= step + 1;
+            if (length == 0)
+                return end;
+            step = std::bit_ceil(length);
+            begin = end - step;
+        }
+        for (step /= 2; step != 0; step /= 2)
+        {
+            if (compare(begin[step], value))
+                begin += step;
+        }
+        return begin + compare(*begin, value);
+    }
+    template<typename It, typename T>
+    It branchless_lower_bound(It begin, It end, const T & value)
+    {
+        return branchless_lower_bound(begin, end, value, std::less<>{});
     }
 
     void Tree::insert(CharOffset offset, std::string_view txt, SuppressHistory suppress_history)
